707a33fd1446120d49ee5cc741585b3ad22935d3
Jimmy Lauchoy <jl7263@nyu.edu>
2019-02-17 09:30:27 -0500

    Fixed map button

--- Screens/ShowQRCodePage.js
+++ Screens/ShowQRCodePage.js
@@ -12,9 +12,9 @@ export default class ShowQRCodePage extends React.Component{
 
   onFinish(){
     const {navigation}=this.props;
-    const numBags=navigation.getParam('numBags', 0);  
-    const dropoff=navigation.getParam('dropoff', 0); 
-    const dropoffPressed=navigation.getParam('dropoffPressed', true);  
+    const numBags=navigation.getParam('numBags', 0);
+    const dropoff=navigation.getParam('dropoff', 0);
+    const dropoffPressed=navigation.getParam('dropoffPressed', true);
     const pickup=navigation.getParam('pickup', 0);
     let out1=dropoff;
     let out2=pickup;
@@ -23,7 +23,7 @@ export default class ShowQRCodePage extends React.Component{
     }else{
       out2=parseInt(out2)+parseInt(numBags);
     }
-  //  this.setState(true)  
+  //  this.setState(true)
     this.props.navigation.navigate('ThankyouPage', {numBags : numBags, 'dropoff': out1,
             'pickup': out2})
   }
@@ -50,12 +50,20 @@ export default class ShowQRCodePage extends React.Component{
       <View style={styles.container}>
         <QRCode
           value={str}
-          size={200}/> 
+          size={200}/>
         <Button
           title={'Finish'}
           style={styles.input}
           onPress={this.onFinish.bind(this)}
-        />       
+        />
+        <Button
+          title={'Go to Map'}
+          titleStyle={{fontWeight: '300', fontSize: 24}}
+          buttonStyle={styles.goToMapButton}
+          style={styles.input}
+          containerStyle={{ marginTop: 20 }}
+          onPress={()=>this.props.navigation.navigate('ShowMapPage')}
+        />
       </View>
     );
   }
@@ -68,5 +76,18 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     backgroundColor: '#aaaaff',
+  },
+  goToMapButton: {
+      //flex: 1,
+      //flexDirection: 'column',
+      alignSelf: "center",
+      backgroundColor: "rgba(0, 70 , 180, 1)",
+      width: 300,
+      height: 50,
+      borderColor: "transparent",
+      borderWidth: 0,
+      borderRadius: 5,
+      marginTop: 100,
+      marginBottom: 10,
   },
 });
